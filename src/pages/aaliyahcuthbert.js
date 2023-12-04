3a5ec5cca5d2601142ad99ab6784600865f935e6
Trinity T <143686700+trinitythan@users.noreply.github.com>
2023-12-03 21:27:37 -0700

    Update and rename TomHolland.js to aaliyahcuthbert.js

--- src/pages/aaliyahcuthbert.js
+++ src/pages/aaliyahcuthbert.js
@@ -6,23 +6,17 @@ export default function examplePage() {
 
         <main>
           <div className='underline'>
-            <h1 className='title'>Hey, I'm Spider man. </h1>
+            <h1 className='title'>Hiiii, I'm Aaliyah! </h1>
           </div>
           <img
             className='pic'
-            src='https://i.postimg.cc/qR2VgDkd/spiderman.jpg'
-            alt='tom holland'
+            src='https://media.licdn.com/dms/image/D4E03AQHKQxNK_xWWWw/profile-displayphoto-shrink_800_800/0/1693693563889?e=1707350400&v=beta&t=dn2Gs4gEbXyBxiYtWbM76xZG0zhZeMzIK8dYS1FAT7Q'
+            alt='aaliyahh'
           />
 
           <p className='description'>
-            Hey I am your friendly neighbourhood Spider Man. My real name is Tom
-            Holland. I am an actor but mostly I fight villains and juggle high school.
-            I am just your average student – well, maybe not so average. 
-            You see, I've got some incredible powers, like superhuman strength, agility, 
-            and the ability to shoot webs from my wrists. With great power comes great 
-            responsibility, and I'm here to protect New York City from all kinds of baddies,
-            from common crooks to supervillains. So, if you ever need a hand or a web-swinging
-            hero in your corner, just remember, I'm your friendly neighborhood Spider-Man!"
+            "Hey i'm aaliyah! I love the color green! I have two pets one cat and one 
+            dog. I'm a cs major. and I love music"
           </p>
 
           <a className='button' href='/'>
